45db37d086fdc0ec2ac9cbe93552560a87d76505
Sonal Somani <smaheshwari31@yahoo.com>
2017-05-22 20:06:32 +0530

    Add files via upload
    Its a simple break timer which will interact with your web browser and play your favourite you tube video in a two hour interval.

---
+++ breaktime.py
@@ -0,0 +1,13 @@
+import webbrowser
+import time
+
+total_breaks=3
+break_count=0
+
+print("This program started on"+time.ctime())
+while(total_breaks<break_count):
+    time.sleep(7200)
+    webbrowser.open("https://www.youtube.com/watch?v=JGwWNGJdvx8",new=2,autoraise=True)
+    break_count=break_count+1
+
+    
